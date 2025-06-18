dada972f854004d6eeefe4e93faffc769ea6dad2
littleshark99 <rafael.clare@hotmail.com>
2025-06-18 20:58:30 -0300

    adicionado o pacote nuget sql e criado a pasta do banco de dados com a classe

--- LanchesMac/LanchesMac/Models/Lanche.cs
+++ LanchesMac/LanchesMac/Models/Lanche.cs
@@ -12,6 +12,7 @@
         public bool IsLanchePreferido { get; set; }
         public bool EmEstoque { get; set; }
 
-        public int CategoriaId  { get; set; }
+        public int CategoriaId { get; set; }
         public virtual Categoria Categoria { get; set; }
+    }
 }
