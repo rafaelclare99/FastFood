8a9e3909a93916a231170adae9693b003ad90f71
littleshark99 <rafael.clare@hotmail.com>
2025-06-24 14:05:24 -0300

    começo do mapeamento do lanche

--- LanchesMac/LanchesMac/Models/Lanche.cs
+++ LanchesMac/LanchesMac/Models/Lanche.cs
@@ -1,12 +1,35 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int LancheId { get; set; }
 
+        [Required(ErrorMessage ="o nome do lanche deve ser informado")]
+        [Display(Name ="nome")]
+        [StringLength(20,ErrorMessage ="o nome tem que ter no maximo 20 caracteres")]
         public string LancheNome { get; set; }
+
+        [Required(ErrorMessage = "a descrição do lanche deve ser informado")]
+        [Display(Name = "Descrição do Lanche")]
+        [MinLength(20,ErrorMessage ="a descrição tem que ter no minimo 20 caracteres")]
+        [MaxLength(70, ErrorMessage = "o nome tem que ter no maximo 70 caracteres")]
         public string DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "a descrição do lanche deve ser informado")]
+        [Display(Name = "Descrição do Lanche Detalhada")]
+        [MinLength(20, ErrorMessage = "a descrição tem que ter no minimo 20 caracteres")]
+        [MaxLength(200, ErrorMessage = "o nome tem que ter no maximo 200 caracteres")]
         public string DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage ="Informe o valor do lanche")]
+        [Display(Name ="Preço")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(1,999.99,ErrorMessage ="O preço deve está entre 1 a 999,999")]
         public decimal Preco { get; set; }
         public string ImgegemUrl { get; set; }
         public string ImagemThumb { get; set; }
