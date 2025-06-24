31b8bb45247f289a52b3d80feb516e474444defc
littleshark99 <rafael.clare@hotmail.com>
2025-06-24 13:50:37 -0300

    criação de data notation e mapeando a tabela

--- LanchesMac/LanchesMac/Models/Categoria.cs
+++ LanchesMac/LanchesMac/Models/Categoria.cs
@@ -1,9 +1,22 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+        [StringLength(100,ErrorMessage = "O tamanho maximo é 100 caracteres")]
+        [Required(ErrorMessage = "informe o nome da categoria")]
+        [Display(Name = "Nome")]
         public string CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho maximo é 100 caracteres")]
+        [Required(ErrorMessage = "informe a descrição da categoria")]
+        [Display(Name = "Nome")]
         public string Descricao { get; set; }
 
         public List<Lanche>Lanches { get; set; }
@@ -11,3 +24,4 @@
 
     }
 }
+ 
