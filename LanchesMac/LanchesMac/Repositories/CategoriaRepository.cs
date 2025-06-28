2bb619097d196d6d838dd4978ceab07f345d9666
littleshark99 <rafael.clare@hotmail.com>
2025-06-27 21:09:18 -0300

    criação do repositorie para categoria

--- LanchesMac/LanchesMac/Repositories/CategoriaRepository.cs
+++ LanchesMac/LanchesMac/Repositories/CategoriaRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace LanchesMac.Repositories
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+
+namespace LanchesMac.Repositories
 {
-    public class CategoriaRepository
+    public class CategoriaRepository : ICategoriaRepository
     {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository (AppDbContext appcontext)
+        {
+            _context = appcontext;
+        }
+
+        public IEnumerable<Categoria> Categorias => _context.Categorias;
     }
 }
