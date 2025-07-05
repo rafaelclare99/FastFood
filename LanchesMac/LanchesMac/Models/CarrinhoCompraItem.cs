7e3d3cf22e91be899197174b2443caefd87f775e
littleshark99 <rafael.clare@hotmail.com>
2025-07-05 17:16:11 -0300

    criação de limpar carrinho compra total e gerar carrinho

--- LanchesMac/LanchesMac/Models/CarrinhoCompraItem.cs
+++ LanchesMac/LanchesMac/Models/CarrinhoCompraItem.cs
@@ -5,7 +5,7 @@ namespace LanchesMac.Models
     public class CarrinhoCompraItem
     {
         public int CarrinhoCompraItemId { get; set; }
-        public Lanche lanche { get; set; }
+        public Lanche Lanche { get; set; }
         public int Quantidade { get; set; }
 
         [StringLength(200)]
