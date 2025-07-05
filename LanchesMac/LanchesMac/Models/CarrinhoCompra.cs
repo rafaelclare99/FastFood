09ed3905b34e8fb219c2471b9b7d5f875af0b54a
littleshark99 <rafael.clare@hotmail.com>
2025-07-05 16:32:02 -0300

    adicionado o metodo para adicionar no carrinho

--- LanchesMac/LanchesMac/Models/CarrinhoCompra.cs
+++ LanchesMac/LanchesMac/Models/CarrinhoCompra.cs
@@ -29,11 +29,47 @@ namespace LanchesMac.Models
             string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
 
             //retorna o carrinho com contexto e o id atibuido ou obtido 
-            return new CarrinhoCompra(context) 
+            return new CarrinhoCompra(context)
             {
                 CarrinhoCompraId = carrinhoId
             };
 
         }
+
+
+        public void AdicionarCarrinho(Lanche lanche)
+        {
+
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(s =>
+            s.lanche.LancheId == lanche.LancheId &&
+            s.CarrinhoCompraId == CarrinhoCompraId);
+
+            if (carrinhoCompraItem == null)
+            {
+
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    lanche = lanche,
+                    Quantidade = 1
+
+                };
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);     
+
+            }
+            else
+            {
+                carrinhoCompraItem.Quantidade++;
+            }
+            _context.SaveChanges();
+
+
+
+
+
+
+
+        }
+
     }
 }
