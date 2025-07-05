6a6ca9f9a4c697a1e042d10698bf862642c3eff0
littleshark99 <rafael.clare@hotmail.com>
2025-07-05 16:17:37 -0300

    criação do metodos do carrinho

---
+++ LanchesMac/LanchesMac/Models/CarrinhoCompra.cs
@@ -0,0 +1,39 @@
+﻿
+
+using LanchesMac.Context;
+
+namespace LanchesMac.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> carrinhoCompraItems { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            //defina uma seção 
+            ISession session =
+                services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //obtem o serviço do contexto
+            var context = services.GetService<AppDbContext>();
+
+            //obtem ou gera o id do carrinho 
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            //retorna o carrinho com contexto e o id atibuido ou obtido 
+            return new CarrinhoCompra(context) 
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+
+        }
+    }
+}
