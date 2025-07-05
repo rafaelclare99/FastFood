2b4396d296567e6bbb2c932f06804e06464fe4a4
littleshark99 <rafael.clare@hotmail.com>
2025-07-05 15:51:07 -0300

    adicionando a requisão http para ser adicionado o carrinho de compra

--- LanchesMac/LanchesMac/Startup.cs
+++ LanchesMac/LanchesMac/Startup.cs
@@ -22,6 +22,7 @@ public class Startup
 
         services.AddTransient<ILanchesRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
         services.AddControllersWithViews();
 
@@ -47,6 +48,7 @@ public class Startup
         app.UseStaticFiles();
 
         app.UseRouting();
+        app.UseSession();
 
         app.UseAuthorization();
 
