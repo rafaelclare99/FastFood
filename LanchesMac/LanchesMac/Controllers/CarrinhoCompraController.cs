f58c7206533a5532b65b1a6a448f56af355a442b
littleshark99 <rafael.clare@hotmail.com>
2025-07-11 16:05:17 -0300

    criação do controlle do carrinho e adicionado na startup

---
+++ LanchesMac/LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -0,0 +1,22 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class CarrinhoCompraController : Controller
+    {
+        private readonly ILanchesRepository _lanchesRepository;
+        private readonly CarrinhoCompraController _carrinhoCompra;
+
+        public CarrinhoCompraController(ILanchesRepository lanchesRepository, CarrinhoCompraController carrinhoCompra)
+        {
+            _lanchesRepository = lanchesRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
