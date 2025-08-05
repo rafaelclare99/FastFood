c007deabfaa631c997bccb8b8555bf1fb87da191
rafaelclare99 <rafael.clare@hotmail.com>
2025-08-05 19:15:47 -0300

    criação do dropdown no menu para filtrar os lanches pela categoria, criação da view default da categoria menu e components de categoria menu para puxar as categorias

---
+++ LanchesMac/LanchesMac/Components/CategoriaMenu.cs
@@ -0,0 +1,26 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository) 
+        {
+          _categoriaRepository = categoriaRepository;      
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categoria = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaId);
+            return View(categoria); 
+        }
+
+
+
+
+
+    }
+}
