2027812980b0f3c917c2f2ea1a424504f7ae4d56
rafaelclare99 <rafael.clare@hotmail.com>
2025-08-19 20:23:01 -0300

    criação da interface dos pedidos e classe dos pedidos

---
+++ LanchesMac/LanchesMac/Repositories/PedidoRepository.cs
@@ -0,0 +1,42 @@
+﻿using AspNetCoreGeneratedDocument;
+using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+
+namespace LanchesMac.Repositories
+{
+    public class PedidoRepository : IPedidoRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly CarrinhoCompra carrinhoCompra;
+
+        public PedidoRepository(AppDbContext appDbContext, CarrinhoCompra carrinhoCompra)
+        {
+            _appDbContext = appDbContext;
+            this.carrinhoCompra = carrinhoCompra;
+        }
+
+        public void CriarPedido(Pedido pedido)
+        {
+            pedido.PedidoEnviado = DateTime.Now;
+            _appDbContext.Pedidos.Add(pedido);
+            _appDbContext.SaveChanges();
+
+            var carrinhoCompraItens = carrinhoCompra.CarrinhoCompraItems;
+            foreach (var carrinhoItem in carrinhoCompraItens)
+            {
+                var pedidoDetail = new PedidoDetalhe()
+                {
+                    Quantidade = carrinhoItem.Quantidade,
+                    LancheId = carrinhoItem.Lanche.LancheId,
+                    PedidoId = pedido.PedidoId,
+                    Preco = carrinhoItem.Lanche.Preco
+
+
+                };
+               _appDbContext.PedidoDetalhes.Add(pedidoDetail);
+            }
+            _appDbContext.SaveChanges();
+        }
+    }
+}
