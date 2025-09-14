d69441019e84e9117f18388043c079f742b8812f
rafaelclare99 <rafael.clare@hotmail.com>
2025-09-14 16:14:58 -0300

    criação do loginviewmoldel

---
+++ LanchesMac/LanchesMac/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o nome")]
+        [Display (Name = "Informe o Usuario ")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Informe o Senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+    }
+}
